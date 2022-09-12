d5c8adf57200b0cd5fca1f87a868156986d7f968
stefano565 <112716138+stefano565@users.noreply.github.com>
2022-09-11 23:49:59 -0500

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,61 @@
+
+# NO MODIFICAR NADA BAJO ESTA LINEA
+def main():
+# NO MODIFICAR NADA SOBRE ESTA LINEA
+
+    #============Pregunta 1============
+    N = input("Nombre: ")
+
+    # Codigo para Pregunta 1 comienza aqui
+
+    print("Bienvenido " + N + "!")
+
+    # Codigo para Pregunta 1 acaba aqui
+
+    #============Pregunta 2============
+
+    N = int(input("N: "))
+
+    # Codigo para Pregunta 2 comienza aqui
+
+    print(N**3-1)
+
+
+    # Codigo para Pregunta 2 acaba aqui
+    #============Pregunta 3============ 
+    
+    N = int(input("N: "))
+    M = int(input("M: "))
+    # Codigo para Pregunta 3 comienza aqui
+
+    print(N**M)
+
+    # Codigo para Pregunta 3 acaba aqui
+    
+
+    #============Pregunta 4============ 
+    
+    N = float(input("N: "))
+
+    # Codigo para Pregunta 4 comienza aqui
+
+    print(N/2.7182)
+
+    # Codigo para Pregunta 4 acaba aqui
+
+    #============Pregunta 5============ 
+    
+    N = str(input("N: "))
+
+    # Codigo para Pregunta 5 comienza aqui
+    if N=="Batman, Superman".split:
+     print("Héroe!")
+    else:
+        print("Villano!!")
+    # Codigo para Pregunta 5 acaba aqui
+
+
+# NO MODIFICAR NADA BAJO ESTA LINEA
+if __name__ == '__main__':
+    main()
+# NO MODIFICAR NADA SOBRE ESTA LINEA
